5eefbda8a6a003ed4c24c1acd533011bb45e44da
gregory <burtelgamerpro@gmail.com>
2025-01-11 03:38:07 +0600

    delete dead code

--- MetaNexus.Lib.NeuralNetwork/Math/Functions/LossFunctions.cs
+++ MetaNexus.Lib.NeuralNetwork/Math/Functions/LossFunctions.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MetaNexus.Lib.NeuralNetwork.Math.Functions
+﻿namespace MetaNexus.Lib.NeuralNetwork.Math.Functions
 {
     public class LossFunctions
     {
