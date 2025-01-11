51909b0cd2a6a4764159892d583c547f1cd43563
gregory <burtelgamerpro@gmail.com>
2025-01-11 15:37:28 +0600

    add demo-app

---
+++ MetaNexus.App.DemoApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MetaNexus.App.DemoApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
