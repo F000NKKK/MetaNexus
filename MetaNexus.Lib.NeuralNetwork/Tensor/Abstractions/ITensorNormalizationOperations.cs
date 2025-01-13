0cf9cdd3c0c54db7c75c5b738ecb8c62e0b0d65d
gregory <burtelgamerpro@gmail.com>
2025-01-14 01:57:36 +0600

    add normalize funcs

--- MetaNexus.Lib.NeuralNetwork/Tensor/Abstractions/ITensorNormalizationOperations.cs
+++ MetaNexus.Lib.NeuralNetwork/Tensor/Abstractions/ITensorNormalizationOperations.cs
@@ -18,5 +18,32 @@
         /// <param name="variance">Дисперсия по батчу.</param>
         /// <returns>Нормализованный тензор по батчу.</returns>
         Tensor BatchNormalize(Tensor mean, Tensor variance);
+
+        /// <summary>
+        /// Нормализация значений тензора в пределах диапазона [0, 1] (Min-Max нормализация).
+        /// </summary>
+        /// <returns>Нормализованный тензор в диапазоне [0, 1].</returns>
+        Tensor MinMaxNormalize();
+
+        /// <summary>
+        /// Нормализация по каналу, используемая для сверток или работы с многоканальными данными.
+        /// </summary>
+        /// <param name="mean">Среднее значение по каналу.</param>
+        /// <param name="variance">Дисперсия по каналу.</param>
+        /// <returns>Нормализованный тензор по каналу.</returns>
+        Tensor ChannelNormalize(Tensor mean, Tensor variance);
+
+        /// <summary>
+        /// Стандартизация значений тензора (централизованная нормализация, с вычитанием среднего и делением на стандартное отклонение).
+        /// </summary>
+        /// <returns>Стандартизованный тензор.</returns>
+        Tensor Standardize();
+
+        /// <summary>
+        /// Лейбловая нормализация, используемая для подготовки данных для классификации.
+        /// </summary>
+        /// <param name="numClasses">Количество классов в задаче классификации.</param>
+        /// <returns>Нормализованный тензор для задачи классификации.</returns>
+        Tensor LabelNormalize(int numClasses);
     }
 }
--- MetaNexus.Lib.NeuralNetwork/Tensor/TensorNormalizationOperations.cs
+++ MetaNexus.Lib.NeuralNetwork/Tensor/TensorNormalizationOperations.cs
@@ -1,17 +1,119 @@
 ﻿using MetaNexus.Lib.NeuralNetwork.Tensor.Abstractions;
+using System;
 
 namespace MetaNexus.Lib.NeuralNetwork.Tensor
 {
     public partial struct Tensor : ITensorNormalizationOperations
     {
-        public Tensor BatchNormalize(Tensor mean, Tensor variance)
-        {
-            throw new NotImplementedException();
-        }
-
         public Tensor Normalize()
         {
-            throw new NotImplementedException();
+            float mean = 0;
+            float variance = 0;
+
+            foreach (var value in _data)
+            {
+                mean += value;
+            }
+            mean /= Size;
+
+            foreach (var value in _data)
+            {
+                variance += (value - mean) * (value - mean);
+            }
+            variance /= Size;
+            float stdDev = (float)Math.Sqrt(variance);
+
+            var result = new Tensor(_shape);
+            for (int i = 0; i < Size; i++)
+            {
+                result._data[i] = (this._data[i] - mean) / stdDev;
+            }
+
+            return result;
+        }
+
+        public Tensor BatchNormalize(Tensor mean, Tensor variance)
+        {
+            if (mean.Size != variance.Size || mean.Size != this.Size)
+                throw new ArgumentException("Размеры тензора, среднего и дисперсии должны совпадать.");
+
+            var result = new Tensor(_shape);
+            for (int i = 0; i < Size; i++)
+            {
+                result._data[i] = (this._data[i] - mean._data[i]) / (float)Math.Sqrt(variance._data[i] + 1e-8f);
+            }
+            return result;
+        }
+
+        public Tensor MinMaxNormalize()
+        {
+            float min = _data[0];
+            float max = _data[0];
+
+            foreach (var value in _data)
+            {
+                if (value < min) min = value;
+                if (value > max) max = value;
+            }
+
+            var result = new Tensor(_shape);
+            for (int i = 0; i < Size; i++)
+            {
+                result._data[i] = (_data[i] - min) / (max - min);
+            }
+
+            return result;
+        }
+
+        public Tensor ChannelNormalize(Tensor mean, Tensor variance)
+        {
+            if (mean.Size != variance.Size || mean.Size != this.Size)
+                throw new ArgumentException("Размеры тензора, среднего и дисперсии должны совпадать.");
+
+            var result = new Tensor(_shape);
+            for (int i = 0; i < Size; i++)
+            {
+                result._data[i] = (this._data[i] - mean._data[i]) / (float)Math.Sqrt(variance._data[i] + 1e-8f);
+            }
+            return result;
+        }
+
+        public Tensor Standardize()
+        {
+            float mean = 0;
+            float variance = 0;
+
+            foreach (var value in _data)
+            {
+                mean += value;
+            }
+            mean /= Size;
+
+            foreach (var value in _data)
+            {
+                variance += (value - mean) * (value - mean);
+            }
+            variance /= Size;
+            float stdDev = (float)Math.Sqrt(variance);
+
+            var result = new Tensor(_shape);
+            for (int i = 0; i < Size; i++)
+            {
+                result._data[i] = (this._data[i] - mean) / stdDev;
+            }
+
+            return result;
+        }
+
+        public Tensor LabelNormalize(int numClasses)
+        {
+            var result = new Tensor(_shape);
+            for (int i = 0; i < Size; i++)
+            {
+                result._data[i] = this._data[i] / numClasses;
+            }
+
+            return result;
         }
     }
 }
