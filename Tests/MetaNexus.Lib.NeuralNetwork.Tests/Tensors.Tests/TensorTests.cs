41bdcfd69c0de07cd451c397c00602661ff98807
gregory <burtelgamerpro@gmail.com>
2025-01-15 00:24:12 +0600

    add tests project

---
+++ Tests/MetaNexus.Lib.NeuralNetwork.Tests/Tensors.Tests/TensorTests.cs
@@ -0,0 +1,15 @@
+﻿namespace MetaNexus.Lib.NeuralNetwork.Tests;
+
+public class TensorTests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
